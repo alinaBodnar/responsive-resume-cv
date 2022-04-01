0a5c252323bdf51a3e4a71975b69b6d7eb2c2211
Bodnar Alina <alina_bodnar2001@yahoo.com>
2022-04-01 14:40:51 +0300

    Media queries and dark theme with Js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@ const showMenu = (toggleId,navId) =>{
     const nav = document.getElementById(navId)
 
     if(toggle && nav){
-        toggle.addEventListener('click',()=>{
+        function show(){
             nav.classList.toggle('show-menu')
-        })
+        }
+        toggle.addEventListener('click',show)
     }
 }
 showMenu('nav-toggle','nav-menu')
@@ -41,3 +42,26 @@ function scrollActive(){
 }
 
 window.addEventListener('scroll',scrollActive)
+
+// dark theme
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme == 'dark' ? 'add': 'remove'](darkTheme)
+    themeButton.classList[selectedIcon == 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+function changeTheme(){
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme',getCurrentTheme())
+    localStorage.setItem('selected-icon',getCurrentIcon())
+}
+themeButton.addEventListener('click',changeTheme)
